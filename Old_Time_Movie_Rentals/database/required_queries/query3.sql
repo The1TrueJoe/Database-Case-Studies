8766210c89ebae362907f416d1c4e0393c9c0d8b
JoeTelaa0 <47280152+The1TrueJoe@users.noreply.github.com>
2021-11-02 11:36:55 -0400

    Add required query skeletons

---
+++ Old_Time_Movie_Rentals/database/required_queries/query3.sql
@@ -0,0 +1,3 @@
+/*
+List the total payemtn received by each employee, sorted by employee number
+*/
---
+++ Old_Time_Movie_Rentals/database/required_queries/query5.sql
@@ -0,0 +1,3 @@
+/*
+List all the movies never rented out in each store, and grouped by store ID ans sorted by movie ID
+*/
