c3d7e343045f0241bd4aee84c753c1c637fbcbac
JoeTelaa0 <47280152+The1TrueJoe@users.noreply.github.com>
2021-11-03 21:54:09 -0400

    Do query 9

--- Old_Time_Movie_Rentals/database/required_queries/query9.sql
+++ Old_Time_Movie_Rentals/database/required_queries/query9.sql
@@ -4,3 +4,28 @@ List the top 5 customers based on their total payment, and sort in descending or
 
 /* Specify the schema to user */
 use CASE03_OTMR;
+
+/* Output customer name, id, and their total payment*/
+SELECT CUSTOMER.CustomerName, CUSTOMER.CustomerID, TOTAL_PAYMENT.PaymentAmount
+/* Access the Employee table */
+FROM CUSTOMER
+/* Join this to a new temp table*/
+JOIN (
+    /* Output the employee ID and the total payment amount into a temp table */
+    SELECT CustomerID, SUM(PaymentAmount) AS PaymentAmount
+    /* Access the PAYMENT table */
+    FROM PAYMENT
+    /* Group multiple payments together based on the ID*/
+    GROUP BY CustomerID
+    )
+
+    /* Set temp table alias to TOTAL_PAYMENT */
+    AS TOTAL_PAYMENT
+    /* Join these tables based on employee IDs*/
+    ON TOTAL_PAYMENT.CustomerID = CUSTOMER.CustomerID
+
+/* Sort based on the IDs */
+ORDER BY TOTAL_PAYMENT.PaymentAmount DESC
+/* Limit to top 5 customers */
+LIMIT 5
+;
