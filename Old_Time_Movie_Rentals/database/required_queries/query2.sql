a92e61e146312cedcfed36ffef233f99a3e60815
JoeTelaa0 <47280152+The1TrueJoe@users.noreply.github.com>
2021-11-03 20:54:42 -0400

    Do query 2

--- Old_Time_Movie_Rentals/database/required_queries/query2.sql
+++ Old_Time_Movie_Rentals/database/required_queries/query2.sql
@@ -4,3 +4,13 @@ List all the customers who lie in UpTown. List their name and address sorted by 
 
 /* Specify the schema to user */
 use CASE03_OTMR;
+
+/* Display customer name and address */
+SELECT CustomerName, CustomerStreet, CustomerState, CustomerZIP
+/* Access the CUSTOMER table */
+FROM CUSTOMER
+/* Exclude customers that are not from UpTown */
+WHERE CustomerCity LIKE 'UpTown'
+/* Sort customers by their names */
+ORDER BY CustomerName
+;
