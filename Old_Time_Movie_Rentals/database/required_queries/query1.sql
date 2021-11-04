c791dde68d9a1ba0e0f146f65867a05aa2310074
JoeTelaa0 <47280152+The1TrueJoe@users.noreply.github.com>
2021-11-03 20:48:23 -0400

    Do query 1

--- Old_Time_Movie_Rentals/database/required_queries/query1.sql
+++ Old_Time_Movie_Rentals/database/required_queries/query1.sql
@@ -1,3 +1,14 @@
 /*
 List the last names of all customers who are now renting
 */
+
+/* Specify the schema to user */
+use CASE03_OTMR;
+
+/* Return the name of the customer */
+SELECT CUSTOMER.CustomerName
+/* Access tables CUSTOMER and MOVIERENTAL */
+FROM CUSTOMER, MOVIERENTAL
+/* Relate the customer IDs of the active rentals to the customers */
+WHERE MOVIERENTAL.CustomerID = CUSTOMER.CustomerID
+;
