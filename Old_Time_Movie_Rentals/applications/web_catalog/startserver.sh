430c032216777f2eb0f48ebf9922278952882357
The1TrueJoe <jttelaak@hotmail.com>
2021-11-18 16:05:45 +0000

    Auto copy script to user

--- Old_Time_Movie_Rentals/applications/web_catalog/startserver.sh
+++ Old_Time_Movie_Rentals/applications/web_catalog/startserver.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
 # Start the web server
+
+# Update app
 cd git/Database-Case-Studies/
 git pull
-cd Old_Time_Movie_Rentals/applications/web_catalog/web-catalog/public
+
+# Copy script to user directory
+cd Old_Time_Movie_Rentals/applications/web_catalog/
+cp startserver.sh ~
+chmod u+x ~/startserver.sh
+
+# Start server
+cd web-catalog/public/
 npm start
