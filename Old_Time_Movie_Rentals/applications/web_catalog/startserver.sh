d7707ca3dce07b2adf447cc2d90067765fc49c8b
The1TrueJoe <jttelaak@hotmail.com>
2021-11-30 16:38:17 +0000

    Fix directory reference

--- Old_Time_Movie_Rentals/applications/web_catalog/startserver.sh
+++ Old_Time_Movie_Rentals/applications/web_catalog/startserver.sh
@@ -2,12 +2,12 @@
 # Start the web server
 
 # Update app
-cd git/Database-Case-Studies/
+cd ~/git/Database-Case-Studies
 git pull
 
 # Copy script to user directory
 cd Old_Time_Movie_Rentals/applications/web_catalog/
-cp startserver.sh ~
+cp startserver.sh ~/
 chmod u+x ~/startserver.sh
 
 # Start server
