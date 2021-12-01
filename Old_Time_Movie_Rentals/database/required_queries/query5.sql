f241274b23cc38fd41166d59786b351191a353db
Akhil <skippynose2@users.noreply.github.com>
2021-11-30 20:01:19 -0500

    Update query5.sql
    Created the requiered 5th query

--- Old_Time_Movie_Rentals/database/required_queries/query5.sql
+++ Old_Time_Movie_Rentals/database/required_queries/query5.sql
@@ -4,3 +4,8 @@ List all the movies never rented out in each store, and grouped by store ID ans 
 
 /* Specify the schema to user */
 use CASE03_OTMR;
+
+SELECT MovieID, StoreID
+FROM MEDIA
+WHERE MediaID NOT IN (SELECT MediaID FROM MOVIERENTAL)
+GROUP BY StoreID, MovieID;
