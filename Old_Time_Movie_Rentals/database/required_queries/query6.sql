d9cd3cf6a4c69a2c610ce01710b4082f057e52c6
Akhil <skippynose2@users.noreply.github.com>
2021-11-30 19:23:05 -0500

    created query06
    Created query06. It now works.

--- Old_Time_Movie_Rentals/database/required_queries/query6.sql
+++ Old_Time_Movie_Rentals/database/required_queries/query6.sql
@@ -4,3 +4,7 @@ List all the customers who did not rent any movie and sort by customer ID
 
 /* Specify the schema to user */
 use CASE03_OTMR;
+SELECT CustomerID, CustomerName
+FROM CUSTOMER
+WHERE CustomerID NOT IN (SELECT CustomerID FROM MOVIERENTAL) 
+GROUP BY CustomerID;
