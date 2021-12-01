676827caf6e255a3e6ca12d151588ba7bbdc687e
Akhil <skippynose2@users.noreply.github.com>
2021-11-30 21:23:58 -0500

    Created Query04
    Query 4 has been created.

--- Old_Time_Movie_Rentals/database/required_queries/query4.sql
+++ Old_Time_Movie_Rentals/database/required_queries/query4.sql
@@ -4,3 +4,8 @@ List the total number of movies rented out by each store, grouped and sorted by 
 
 /* Specify the schema to user */
 use CASE03_OTMR;
+
+SELECT MOVIERENTAL.StoreID, COUNT(MOVIERENTAL.StoreID), STORE.StoreName
+FROM MOVIERENTAL, STORE
+WHERE MOVIERENTAL.StoreID = STORE.StoreID
+GROUP BY StoreID;
