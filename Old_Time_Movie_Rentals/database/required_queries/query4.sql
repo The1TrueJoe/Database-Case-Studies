bd565e521db2b56ddbbb898bf69bc8e8530bcc8f
Akhil <skippynose2@users.noreply.github.com>
2021-11-30 21:33:47 -0500

    Naming
    Redid some naming.

--- Old_Time_Movie_Rentals/database/required_queries/query4.sql
+++ Old_Time_Movie_Rentals/database/required_queries/query4.sql
@@ -5,7 +5,7 @@ List the total number of movies rented out by each store, grouped and sorted by 
 /* Specify the schema to user */
 use CASE03_OTMR;
 
-SELECT MOVIERENTAL.StoreID, COUNT(MOVIERENTAL.StoreID), STORE.StoreName
+SELECT MOVIERENTAL.StoreID, COUNT(MOVIERENTAL.StoreID) AS NumberOfRentedOutMovies, STORE.StoreName
 FROM MOVIERENTAL, STORE
 WHERE MOVIERENTAL.StoreID = STORE.StoreID
 GROUP BY StoreID;
