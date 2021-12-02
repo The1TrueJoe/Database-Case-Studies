eb31c6d0dd6d838b8b3466973504b43302b16574
8enji <58536087+8enji@users.noreply.github.com>
2021-12-02 11:55:27 -0500

    Query 7
    Finished query 7. All queries done now.

--- Old_Time_Movie_Rentals/database/required_queries/query7.sql
+++ Old_Time_Movie_Rentals/database/required_queries/query7.sql
@@ -1,6 +1,6 @@
-/*
-List the total amount received by each payment type, sort by payment description
-*/
+-- query 7: list the toal amount received by each payment, sort by payment description
+select PAYMENTTYPE.Description, sum(PAYMENT.PaymentAmount) as Amount_Received from PAYMENT
+join PAYMENTTYPE on PAYMENT.PaymentID = PAYMENTTYPE.PaymentID
+group by PAYMENTTYPE.Description
+order by PAYMENTTYPE.Description
 
-/* Specify the schema to user */
-use CASE03_OTMR;
