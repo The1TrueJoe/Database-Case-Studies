3088cd4af1e2bfadae2bd98a13a7c345a992bee3
The1TrueJoe <jttelaak@hotmail.com>
2021-12-02 13:15:50 -0500

    Comment queries

--- Old_Time_Movie_Rentals/applications/backend-api/backend-api/server.py
+++ Old_Time_Movie_Rentals/applications/backend-api/backend-api/server.py
@@ -19,16 +19,16 @@ app = Flask(__name__)
 
 # Required Query Map
 req_query_opt = {
-        "query1": rquery.query1,
-        "query2": rquery.query2,
-        "query3": rquery.query3,
-        "query4": rquery.query4,
-        "query5": rquery.query5,
-        "query6": rquery.query6,
-        "query7": rquery.query7,
-        "query8": rquery.query8,
-        "query9": rquery.query9,
-        "query10": rquery.query10,
+        "query1": rquery.query1,    # List the last names of all customers who are now renting
+        "query2": rquery.query2,    # List all the customers who lie in UpTown. List their name and address sorted by name
+        "query3": rquery.query3,    # List the total payment received by each employee, sorted by employee number
+        "query4": rquery.query4,    # List the total number of movies rented out by each store, grouped and sorted by store ID
+        "query5": rquery.query5,    # List all the movies never rented out in each store, and grouped by store ID ans sorted by movie ID
+        "query6": rquery.query6,    # List all the customers who did not rent any movie and sort by customer ID
+        "query7": rquery.query7,    # List the total amount received by each payment type, sort by payment description
+        "query8": rquery.query8,    # Display the number of movies rented out based on the movie genre, sort by genre
+        "query9": rquery.query9,    # List the top 5 customers based on their total payment, and sort in descending order by total payment
+        "query10": rquery.query10,  # List all the managers and the names of the employees they manage. Sort by manager ID then by employee ID
         "test": "USE Test1; SELECT * FROM TESTTBL;"
 
 }
