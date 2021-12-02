df26b2675e8b9df785bac0a372269a6b08aedf6c
Akhil <skippynose2@users.noreply.github.com>
2021-12-02 11:10:56 -0500

    Commented all the queries
    Commented all the queries

--- Old_Time_Movie_Rentals/database/required_queries/query4.sql
+++ Old_Time_Movie_Rentals/database/required_queries/query4.sql
@@ -5,7 +5,14 @@ List the total number of movies rented out by each store, grouped and sorted by 
 /* Specify the schema to user */
 use CASE03_OTMR;
 
+/*	Access StoreID values of the MOVIERENTAL table and counts the number of times a unique StoreID occurs in MOVIERENTAL. The amount of times that StoreID occurs
+	is the number of movies that store has rented out. Here we are also grabbing the StoreName from the STORE table
+*/
 SELECT MOVIERENTAL.StoreID, COUNT(MOVIERENTAL.StoreID) AS NumberOfRentedOutMovies, STORE.StoreName
 FROM MOVIERENTAL, STORE
+
+/*Making sure that the StoreID from both tables match up*/
 WHERE MOVIERENTAL.StoreID = STORE.StoreID
+
+/*Grouping by StoreID*/
 GROUP BY StoreID;
