0438649c554ceafc7892e88aa2442f10a100f82f
8enji <58536087+8enji@users.noreply.github.com>
2021-12-02 11:51:48 -0500

    Query 8, 10
    Finished query 8 and 10. Someone should double check.

--- Old_Time_Movie_Rentals/database/required_queries/query10.sql
+++ Old_Time_Movie_Rentals/database/required_queries/query10.sql
@@ -1,6 +1,8 @@
-/*
-List all the managers and the names of the employees they manage. Sort by manager ID then by employee ID
-*/
-
-/* Specify the schema to user */
+-- qeury 10: List all managers and the employees they manage. Sort by ManagerSIN, then by EmployeeSIN
 use CASE03_OTMR;
+select e.EmployeeName as Manager, e2.EmployeeName as Employee
+from EMPLOYEE e, EMPLOYEE e2
+where e.ManagerSIN = e.EmployeeSIN
+order by e.ManagerSIN, e2.EmployeeSIN
+
+
--- Old_Time_Movie_Rentals/database/required_queries/query8.sql
+++ Old_Time_Movie_Rentals/database/required_queries/query8.sql
@@ -1,6 +1,6 @@
-/*
-Display the number of movies rented out based on the movie genre, sort by genre
-*/
+-- query 8: Display number of movies rented out by genre, sort by genre
+select MOVIE.Genre, count(genre) as Total_Rented from MOVIERENTAL
+join MOVIE on MOVIERENTAL.MovieID = MOVIE.MovieID
+group by MOVIE.Genre
+order by MOVIE.Genre;
 
-/* Specify the schema to user */
-use CASE03_OTMR;
