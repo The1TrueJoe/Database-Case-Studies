6ecfd71d53a07a529a82533254df6d9535ef2f8d
The1TrueJoe <jttelaak@hotmail.com>
2021-12-07 16:31:48 +0000

    Update about page

--- Old_Time_Movie_Rentals/applications/web_catalog/web-catalog/src/pages/About.js
+++ Old_Time_Movie_Rentals/applications/web_catalog/web-catalog/src/pages/About.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
-const GridWrapper = styled.div`
-  display: grid;
-  grid-gap: 10px;
+
+const Wrapper = styled.div`
   margin-top: 1em;
   margin-left: 6em;
   margin-right: 6em;
-  grid-template-columns: repeat(12, 1fr);
-  grid-auto-rows: minmax(25px, auto);
-`; 
+`;
+
 export const About = () => (
-  <GridWrapper>
-    <h2>About Page</h2>
-    <p>State at ceiling lay on arms while you're using the keyboard so this human feeds me.</p>
-    <p>I am a kitty cat, sup, feed me, no cares in the world</p>
-    <p>Meow meow, I tell my human purr for no reason but to chase after</p>
-  </GridWrapper>
+  <Wrapper>
+    <div>
+      <h2>About Page</h2>
+      <br></br> 
+    </div>
+    <div>
+      <p>
+        As a company, JAB strives to fulfill all of our customers’ needs efficiently and effectively. JAB
+        was founded as a start-up in 2021 by three young ambitious entrepreneurs and database
+        designers. Even though the company is rather new, it has embarked on database projects for
+        over 50 different companies, ranging from your local grocery store to companies as large as
+        Target. All of said companies have left positive reviews regarding the service, proving the
+        effectiveness of JAB’s designs. Moving into the future, JAB’s mission is to continue supporting
+        companies by designing databases that can allow the company to prosper.
+      </p>
+      <br></br>
+    </div>
+    <div>
+      <embed src="https://the1truejoe.github.io/Database-Case-Studies/Old_Time_Movie_Rentals/documents/Cover%20Letter.pdf" width="800px" height="800px" />
+      <embed src="https://the1truejoe.github.io/Database-Case-Studies/Old_Time_Movie_Rentals/documents/Executive%20Summary.pdf" width="800px" height="800px" />
+    </div>
+  </Wrapper>
 )
