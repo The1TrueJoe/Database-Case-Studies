4eb72c78233decb0d4f8010461938d519c7b05b0
The1TrueJoe <jttelaak@hotmail.com>
2021-12-09 06:06:25 +0000

    Fix home page

--- Old_Time_Movie_Rentals/applications/web_catalog/web-catalog/src/pages/Home.js
+++ Old_Time_Movie_Rentals/applications/web_catalog/web-catalog/src/pages/Home.js
@@ -14,8 +14,10 @@ const Wrapper = styled.div`
 
 // Retuen page
 export const Home = (props) => (
-  <div style = "height: 100vh; display: flex; justify - content: center;align - items: center;">
+  <Wrapper>
+    <div>
         <h1>WELCOME</h1>
         <h2>Welcome to the home page of the web app. On this web app you can find reports for all of the 10 engineered queries. This database solution was designed by JAB solutions</h2>
-  </div>
+    </div>
+  </Wrapper>
 )
