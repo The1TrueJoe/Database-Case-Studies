d2cd074c3b35ed905fdcc0d7697fccc231a1dc4a
The1TrueJoe <jttelaak@hotmail.com>
2021-12-09 05:38:54 +0000

    Change button names

--- Old_Time_Movie_Rentals/applications/web_catalog/web-catalog/src/pages/Reports.js
+++ Old_Time_Movie_Rentals/applications/web_catalog/web-catalog/src/pages/Reports.js
@@ -29,34 +29,34 @@ export const Reports = () => (
     </div>
     <div>
       <Styles>
-        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/1/pdf">Report 1</Button>
+        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/1/pdf">List the last names of all customers who are now renting</Button>
       </Styles>
       <Styles>
-        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/2/pdf">Report 2</Button>
+        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/2/pdf">List all the customers who lie in UpTown. List their name and address sorted by name</Button>
       </Styles>
       <Styles>
-        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/3/pdf">Report 3</Button>
+        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/3/pdf">List the total payment received by each employee, sorted by employee number</Button>
       </Styles>
       <Styles>
-        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/4/pdf">Report 4</Button>
+        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/4/pdf">List the total number of movies rented out by each store, grouped and sorted by store ID</Button>
       </Styles>
       <Styles>
-        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/5/pdf">Report 5</Button>
+        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/5/pdf">List all the movies never rented out in each store, and grouped by store ID ans sorted by movie ID</Button>
       </Styles>
       <Styles>
-        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/6/pdf">Report 6</Button>
+        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/6/pdf">List all the customers who did not rent any movie and sort by customer ID</Button>
       </Styles>
       <Styles>
-        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/7/pdf">Report 7</Button>
+        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/7/pdf">List the total amount received by each payment type, sort by payment description</Button>
       </Styles>
       <Styles>
-        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/8/pdf">Report 8</Button>
+        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/8/pdf">Display the number of movies rented out based on the movie genre, sort by genre</Button>
       </Styles>
       <Styles>
-        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/9/pdf">Report 9</Button>
+        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/9/pdf">List the top 5 customers based on their total payment, and sort in descending order by total payment</Button>
       </Styles>
       <Styles>
-        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/10/pdf">Report 10</Button>
+        <Button variant="primary" href="http://127.0.0.1:5000/required_queries/10/pdf">List all the managers and the names of the employees they manage. Sort by manager ID then by employee ID</Button>
       </Styles>
 
       
