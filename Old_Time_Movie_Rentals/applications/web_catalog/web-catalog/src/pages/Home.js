c4008bdf9336fc70c971a1ac94e1535d39e1997a
The1TrueJoe <jttelaak@hotmail.com>
2021-12-09 06:02:22 +0000

    Home page

--- Old_Time_Movie_Rentals/applications/web_catalog/web-catalog/src/pages/Home.js
+++ Old_Time_Movie_Rentals/applications/web_catalog/web-catalog/src/pages/Home.js
@@ -15,7 +15,14 @@ const Wrapper = styled.div`
 // Retuen page
 export const Home = (props) => (
   <Wrapper>
-    <p>This is a paragraph and I am writing on the home page</p>
-    <p>This is another paragraph, hi hey hello whatsup yo</p>
+    <p>
+        As a company, JAB strives to fulfill all of our customers’ needs efficiently and effectively. JAB
+        was founded as a start-up in 2021 by three young ambitious entrepreneurs and database
+        designers. Even though the company is rather new, it has embarked on database projects for
+        over 50 different companies, ranging from your local grocery store to companies as large as
+        Target. All of said companies have left positive reviews regarding the service, proving the
+        effectiveness of JAB’s designs. Moving into the future, JAB’s mission is to continue supporting
+        companies by designing databases that can allow the company to prosper.
+      </p>
   </Wrapper>
 )
