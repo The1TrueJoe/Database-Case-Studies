5cc3738b4c27d3e18c3dbd85de6088e045745256
The1TrueJoe <jttelaak@hotmail.com>
2021-12-10 16:36:40 -0500

    Fix scripts

--- Old_Time_Movie_Rentals/applications/backend-api/run.sh
+++ Old_Time_Movie_Rentals/applications/backend-api/run.sh
@@ -1,6 +1,4 @@
-cd backend-api/
 export FLASK_APP=server.py
 export FLASK_ENV=development
 flask run
 #flask run --host=0.0.0.0 --port=5000
-cd ..
--- Old_Time_Movie_Rentals/applications/web_catalog/startserver.sh
+++ Old_Time_Movie_Rentals/applications/web_catalog/startserver.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 # Start the web server
 
-# Update app
-cd ~/git/Database-Case-Studies
-git pull
-
-# Copy script to user directory
-cd Old_Time_Movie_Rentals/applications/web_catalog/
-cp startserver.sh ~/
-chmod u+x ~/startserver.sh
-
 # Start server
-cd web-catalog/public/
 npm start
